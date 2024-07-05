9a4044929ba17c302b17f59d375ab77de21610b1
Jonathan Engdahl <jrengdahl@gmail.com>
2024-07-05 00:42:57 -0400

    Added SV code for DMA and bus master.
    Some reorg and cleanup of qbus.sv.
    Added constraint (SDC) file.
    Compiles without error, but not tested yet.

--- qbus/qbus.sv
+++ qbus/qbus.sv
@@ -23,6 +23,7 @@ module qbus (
     input logic BRPLYf,
     input logic BWTBTf,
     input logic BBS7f,
+    input logic BDMGIf,
     input logic BINITf,
     output logic Outbound,
     output logic BRPLYg,
@@ -49,66 +50,110 @@ module qbus (
     output logic dummy
     );
 
+    // address of registers as seen from the H723
+    parameter [21:0] FADDR_IR   = 0;
+    parameter [21:0] FADDR_SA   = 2;
+    parameter [21:0] FADDR_CT   = 4;
+    parameter [21:0] FADDR_LO   = 6;
+    parameter [21:0] FADDR_HI   = 8;
+    parameter [21:0] FADDR_DATA_OUT = 10;
+    parameter [21:0] FADDR_DATA_IN = 12;
+
+    // addresses of registers as seen from the PDP-11
     parameter [21:0] QADDR_IR = 22'o17772150;
     parameter [21:0] QADDR_SA = 22'o17772152;
+
     
     
+    // RQDX3 compatible registers
     logic [15:0] SA_Status;         // status read by PDP-11, written by H723
     logic [15:0] SA_Address;        // address written by PDP-11, read by H723
 
+
+    // registers used by the H723 to access Qbus via DMA
+    logic [14:0] Q_Ctl;             // bus control, written by H723
+    logic [21:0] Q_Addr;            // bus address
+    logic [15:0] Q_Data_out;        // bus data output
+    logic [15:0] Q_Data_in;         // bus data output
+    
+    
+    // status bits set by PDP-11 activity, read by the H723
     logic IR_Read;                  // IR register has been read by PDP-11 (poll queue)
     logic IR_Written;               // IR register has been written by PDP-11 (init)
     logic SA_Read;                  // SA register has been read by PDP-11
     logic SA_Written;               // SA register has been written by PDP-11
-
-    logic IR_ReadR;                 // latched version of the above for read-and-clear
+    logic BRPLYL_Asserted;          // BRPLYL has been asserted
+    logic BRPLYL_Deasserted;        // BRPLY has been deasserted
+    logic BSACKL_Asserted;          // BSACKL has been asserted
+    
+    // latched version of the above for read-and-clear
+    logic IR_ReadR;                 //
     logic IR_WrittenR;              //
     logic SA_ReadR;                 //
     logic SA_WrittenR;              //
+    logic BRPLYL_AssertedR;         //
+    logic BRPLYL_DeassertedR;       //
+    logic BSACKL_AssertedR;         //
 
+    // Both the Qbus and FMC bus have multiplexed address/data
+    // these are the addresses latched during the address phase of a bbus cycle for both busses
     logic [23:0] Faddress;          // Latch for the FMC address
     logic Q_IR_selected;            // latches whether the IR register is addressed by the Qbus
     logic Q_SA_selected;            // latches whether the SA register is addressed by the Qbus
     logic Qaddress0;                // latches the low bit of the Qbus address
-       
+
+    
+    // interrupt the H723 if the PDP-11 has read or written any register
     assign FPGA_IRQ = IR_Read || IR_Written || SA_Read || SA_Written;
 
+    // IR is being read by H723
+    wire F_IR_read_enable = !NE1 && !NOE && Faddress[21:0] == FADDR_IR[21:0];
+  
 
-    assign LED = SA_Address[0];
-    assign PLL_RSTN = 1;
-   
-    logic  F_IR_read_selected;      // comb, IR is being read by H723
-    assign F_IR_read_selected = !NE1 && !NOE && Faddress[21:0] == QADDR_IR[21:0];
-
- 
-    assign BSYNCg = 0;
-    assign BDINg  = 0;
-    assign BDOUTg = 0;
-    assign BWTBTg = 0;
-    assign BBS7g  = 0;
-    assign BIRQ4g = 0;
-    assign BIRQ5g = 0;
-    assign BIRQ6g = 0;
     assign BIAKOg = 0;
-    assign BDMRg  = 0;
-    assign BSACKg = 0;
-    assign BDMGOg = 0;
-    assign BREFg  = 0;
 
-
+    assign PLL_RSTN = 1;            // the PLL needs this held high
+    
+    
+///////////////////////////////////////////
+///
+///  FMC operations
+///
+///////////////////////////////////////////
+    
+    
     // Latch the FMC address on NL
     always_ff @(posedge NL)
         begin
         Faddress <= {A, DA_IN, !NBL1 && NBL0};
         end
 
-    // FMC write to SA register
+    // FMC write
     always_ff @(posedge NWE)
         begin
-        if (!NE1 && Faddress[21:1] == QADDR_SA[21:1])
+        if (!NE1 && Faddress[21:1] == FADDR_SA[21:1])
             begin
             if (!NBL0) SA_Status[7:0]  <= DA_IN[7:0];   // Byte 0 write
             if (!NBL1) SA_Status[15:8] <= DA_IN[15:8];  // Byte 1 write
+            end
+        else if (!NE1 && Faddress[21:1] == FADDR_CT[21:1])
+            begin
+            if (!NBL0) Q_Ctl[7:0]  <= DA_IN[7:0];   // Byte 0 write
+            if (!NBL1) Q_Ctl[14:8] <= DA_IN[14:8];  // Byte 1 write
+            end
+        else if (!NE1 && Faddress[21:1] == FADDR_LO[21:1])
+            begin
+            if (!NBL0) Q_Addr[7:0]  <= DA_IN[7:0];   // Byte 0 write
+            if (!NBL1) Q_Addr[15:8] <= DA_IN[15:8];  // Byte 1 write
+            end
+        else if (!NE1 && Faddress[21:1] == FADDR_HI[21:1])
+            begin
+            if (!NBL0) Q_Addr[21:16]  <= DA_IN[5:0];   // Byte 0 write
+            end
+        else if (!NE1 && Faddress[21:1] == FADDR_DATA_OUT[21:1])
+            begin
+            if (!NBL0) Q_Data_out[7:0]  <= DA_IN[7:0];   // Byte 0 write
+            if (!NBL1) Q_Data_out[15:8] <= DA_IN[15:8];  // Byte 1 write
             end
         end
 
@@ -120,6 +165,9 @@ module qbus (
         IR_WrittenR <= IR_Written;
         SA_ReadR <= SA_Read;
         SA_WrittenR <= SA_Written;
+        BRPLYL_AssertedR <= BRPLYL_Asserted;
+        BRPLYL_DeassertedR <= BRPLYL_Deasserted;
+        BSACKL_AssertedR <= BSACKL_Asserted;
         end
     
     always_comb
@@ -129,20 +177,52 @@ module qbus (
         
         if (!NE1 && !NOE)
             begin
-            if(Faddress[21:1] == QADDR_IR[21:1])
+            DA_OE = 16'hFFFF;
+
+            if(Faddress[21:1] == FADDR_IR[21:1])
                 begin
-                DA_OUT = {12'b0, SA_WrittenR, SA_ReadR, IR_WrittenR, IR_ReadR};    // Drive the AD bus with register data
-                DA_OE = 16'hFFFF;
+                DA_OUT = {8'b0,     // Drive the AD bus with register data
+                        BSACKL_AssertedR,
+                        BRPLYL_DeassertedR,
+                        BRPLYL_AssertedR,
+                        ~BRPLYf,
+                        SA_WrittenR,
+                        SA_ReadR, 
+                        IR_WrittenR, 
+                        IR_ReadR};
                 end
-            else if(Faddress[21:1] == QADDR_SA[21:1])
+            else if(Faddress[21:1] == FADDR_SA[21:1])
                 begin
                 DA_OUT = SA_Address;    // Drive the AD bus with register data
-                DA_OE = 16'hFFFF;
+                end
+            else if(Faddress[21:1] == FADDR_CT[21:1])
+                begin
+                DA_OUT = {1'b0, Q_Ctl};    // Drive the AD bus with register data
+                end
+            else if(Faddress[21:1] == FADDR_LO[21:1])
+                begin
+                DA_OUT = Q_Addr[15:0];    // Drive the AD bus with register data
+                end
+            else if(Faddress[21:1] == FADDR_HI[21:1])
+                begin
+                DA_OUT = {10'b0, Q_Addr[21:16]};    // Drive the AD bus with register data
+                end
+            else if(Faddress[21:1] == FADDR_DATA_OUT[21:1])
+                begin
+                DA_OUT = Q_Data_out;    // Drive the AD bus with register data
+                end
+            else if(Faddress[21:1] == FADDR_DATA_IN[21:1])
+                begin
+                DA_OUT = Q_Data_in;    // Drive the AD bus with register data
                 end
             end
         end
 
-
+///////////////////////////////////////////
+///
+///  QBUS operations as bus slave
+///
+///////////////////////////////////////////
 
     // Capture QBus address and related info at leading edge of BSYNC
     always_ff @(negedge BSYNCf or negedge BINITf)
@@ -162,9 +242,9 @@ module qbus (
         end
 
     // QBus write
-    always_ff @(posedge BDOUTf or posedge F_IR_read_selected)
+    always_ff @(posedge BDOUTf or posedge F_IR_read_enable)
         begin
-        if (F_IR_read_selected)
+        if (F_IR_read_enable)
             begin
             IR_Written <= 0;
             SA_Written <= 0;
@@ -182,9 +262,9 @@ module qbus (
         end
 
     // QBus read, clocked part
-    always_ff @(posedge BDINf or posedge F_IR_read_selected)
+    always_ff @(posedge BDINf or posedge F_IR_read_enable)
         begin
-        if (F_IR_read_selected)
+        if (F_IR_read_enable)
             begin
             IR_Read <= 0;
             SA_Read <= 0;
@@ -199,17 +279,38 @@ module qbus (
             end
         end
 
-    // Qbus read operation, and write BRPLY, combinatorial part
+
+    wire Q_Addr_enable = Q_Ctl[10];
+    
+    // Qbus read as slave, and DMA write as master
     always_comb
         begin
 
-        BRPLYg = 0;
         BDALf_OUT = 22'h000000;
         BDALf_OE  = 22'h000000;                         // disable the FPGA bus drivers by default
         Outbound = 0;                                   // disable the BDAL gate drivers by default
         
+        if (BSACKg)
+            begin
+            if (Q_Addr_enable)
+                begin
+                BDALf_OUT = Q_Addr;
+                BDALf_OE = 22'h3FFFFF;                      // enable the FPGA bus drivers to output the data
+                Outbound = 1;                               // enable the gate drivers
+                end
+            else if(BDOUTg)
+                begin
+                BDALf_OUT[21:18] = 4'b0000;
+                BDALf_OUT[17] = 0;                          // memory parity error enable
+                BDALf_OUT[16] = 0;                          // memory parity error
+                BDALf_OUT[15:0] = Q_Data_out;               // Drive the BDALf bus with register data
+                BDALf_OE = 22'h3FFFFF;                      // enable the FPGA bus drivers to output the data
+                Outbound = 1;                               // enable the gate drivers
+                end
+            end
+
         // Qbus read operation
-        if (Q_IR_selected && !BDINf)
+        else if (Q_IR_selected && !BDINf)
             begin
             BDALf_OE = 22'h3FFFFF;                      // enable the FPGA bus drivers to output the data
             Outbound = 1;                               // enable the gate drivers
@@ -225,16 +326,102 @@ module qbus (
             Outbound = 1;                               // enable the gate drivers
             end
             
-        // assert BRPLY as needed
-        if ((Q_IR_selected || Q_SA_selected) && (!BDINf || !BDOUTf))
-            begin
-            BRPLYg = 1;                                 // assert the reply signal
-            end
-
         end          
+
+        
+    // assert BRPLY as needed
+    assign BRPLYg = (Q_IR_selected || Q_SA_selected) && (!BDINf || !BDOUTf);
 
 
     
+///////////////////////////////////////////
+///
+///  Qbus operations as bus master
+///
+///////////////////////////////////////////
+
+
+    // the Q_Ctl register enables the H723 to control the bus master outputs
+    assign BSYNCg = Q_Ctl[0];
+    assign BDINg  = Q_Ctl[1];
+    assign BDOUTg = Q_Ctl[2];
+    assign BWTBTg = Q_Ctl[3];
+    assign BDMRg  = Q_Ctl[4];
+    assign BREFg  = Q_Ctl[5];
+    assign BBS7g  = Q_Ctl[6];
+    assign BIRQ4g = Q_Ctl[7];
+    assign BIRQ5g = Q_Ctl[8];
+    assign BIRQ6g = Q_Ctl[9];
+
+    
+    // Detect assertion of BRPLYL
+    always_ff @(negedge BRPLYf or posedge F_IR_read_enable)
+        begin
+        if (F_IR_read_enable)   BRPLYL_Asserted <= 0;
+        else                    BRPLYL_Asserted <= 1;
+        end
+
+    // Detect deassertion of BRPLYL
+    always_ff @(posedge BRPLYf or posedge F_IR_read_enable)
+        begin
+        if (F_IR_read_enable)   BRPLYL_Deasserted <= 0;
+        else                    BRPLYL_Deasserted <= 1;
+        end
+
+        
+    
+    // When a DMA grant in is received,
+    // and DMA has not been requested,
+    // and DMA is not already in progress,
+    // then relay the grant on down the chain.
+    
+    // When a DMA grant in is received,
+    // and DMA has been requested,
+    // and DMA is not already in progress,
+    // then assert DMA_grant. The leading edge of this
+    // signal is used a few lines later to set the BSACKg flip-flip.
+    
+
+    assign BDMGOg = !BDMGIf && !BDMRg && !BSACKg;
+    wire DMA_grant = !BDMGIf && BDMRg && BSYNCf && BRPLYf;
+    wire DMA_done = !NE1 && !NWE && Faddress[21:1] == FADDR_CT[21:1] && !NBL1 && DA_IN[15];
+
+
+    // when DMA_grant is received, assert BSACKL, and keep on asserting it until the H723 says DMA is done
+    always_ff @(posedge DMA_grant or posedge DMA_done)
+        begin
+        if(DMA_done)BSACKg <= 0;
+        else        BSACKg <= 1;
+        end
+
+    // Detect assertion of BSACKL
+    always_ff @(posedge BSACKg or posedge F_IR_read_enable)
+        begin
+        if (F_IR_read_enable)   BSACKL_Asserted <= 0;
+        else                    BSACKL_Asserted <= 1;
+        end
+
+
+    // capture data read from Qbus
+    always @(negedge BDINg)
+        begin
+        Q_Data_in[15:0] <= !BDALf_IN[15:0];
+        end
+        
+
+     
+     
+        
+///////////////////////////////////////////
+///
+///  Debug stuff
+///
+///////////////////////////////////////////
+
+
+
+    assign LED = SA_Address[0];     // light LED for testing
+       
     // do something silly with the clock to make sure it is not optimized away
     // so we can use it for the logic analyzer
     always_ff @(posedge clock)
